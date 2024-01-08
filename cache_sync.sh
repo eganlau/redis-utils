272b6a8f6ba164b4a053194088f6d0a1d1a8a7c6
Egan Lau <eganlau@hotmail.com>
2024-01-08 17:59:41 +0800

    sync both ways

--- cache_sync.sh
+++ cache_sync.sh
@@ -2,5 +2,9 @@ source .env
 cd migrate/
 ./download.js -h $SOURCE_HOSTNAME -p $SOURCE_PORT -a $SOURCE_PASSWORD -n $SOURCE_DB --pattern '*'
 ./upload.js -h $DEST_HOSTNAME -p $DEST_PORT -n $DEST_DB
+
+./download.js -h $DEST_HOSTNAME -p $DEST_PORT -n $DEST_DB --pattern '*'
+./upload.js -h $SOURCE_HOSTNAME -p $SOURCE_PORT -a $SOURCE_PASSWORD -n $SOURCE_DB
+
 cd ../count/
 ./count.js -h $DEST_HOSTNAME -p $DEST_PORT -n $DEST_DB
