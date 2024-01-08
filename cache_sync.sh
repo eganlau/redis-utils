a9c850dcffa070c733c8a75d856718f18bbb8fc5
Egan Lau <eganlau@hotmail.com>
2024-01-08 16:50:45 +0800

    correct error

--- cache_sync.sh
+++ cache_sync.sh
@@ -1,6 +1,6 @@
 source .env
 cd migrate/
-download.js -h $SOURCE_HOSTNAME -p $SOURCE_PORT -a $SOURCE_PASSWORD -n $SOURCE_DB --pattern '*'
-upload.js -h $DEST_HOSTNAME -p $DEST_PORT -n $DEST_DB
+./download.js -h $SOURCE_HOSTNAME -p $SOURCE_PORT -a $SOURCE_PASSWORD -n $SOURCE_DB --pattern '*'
+./upload.js -h $DEST_HOSTNAME -p $DEST_PORT -n $DEST_DB
 cd ../count/
-count.js -h $DEST_HOSTNAME -p $DEST_PORT -n $DEST_DB
+./count.js -h $DEST_HOSTNAME -p $DEST_PORT -n $DEST_DB
